75f2cf7c99f51353a328c1a8f03906e2044ff990
Nima Azbijari <nazbijar@ucsd.edu>
2019-06-12 18:29:27 -0700

    Segment videos to jpeg

---
+++ segment.py
@@ -0,0 +1,27 @@
+
+import cv2
+from pathlib import Path
+
+
+data_path = Path("C:\\Users\\nazbi\\OneDrive\\Desktop\\speedchallenge\\data")
+PATH = data_path / "train.mp4"
+vidcap = cv2.VideoCapture(str(PATH))
+success, image = vidcap.read()
+
+count = 0
+while success:
+    cv2.imwrite("train-frames/frame%d.jpg" % count, image)
+    success, image = vidcap.read()
+    count += 1
+
+assert count == 20400
+
+count = 0
+vidcap = cv2.VideoCapture('data/test.mp4')
+success, image = vidcap.read()
+
+while success:
+    cv2.imwrite("test-frames/frame%d.jpg" % count, image)
+    success, image = vidcap.read()
+    count += 1
+
